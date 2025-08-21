373de74fe045a3eba47cbd613c724c708db53c4f
Connorw08 <111461534+Connorw08@users.noreply.github.com>
2025-08-21 16:24:11 -0400

    Updated Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,20 +5,27 @@ const Experience = () => {
   const jobs = [
     {
       id: 1,
+      title: 'Software Engineer Intern',
+      company: 'Parsons Corporation',
+      period: 'May 2025 - Present',
+      description: 'Lead frontend development efforts for a data visualization web application, implementing complex React components with intuitive, modern interface designs. Utilize React Flow to create an interactive flow editor feature, enabling users to design and visualize data workflows. Optimize file storage system by implementing a file streaming algorithm, reducing backend storage requirements and improving data transfer efficiency.'
+    },
+    {
+      id: 2,
       title: 'Program Assistant',
       company: 'BeAM UNC, Chapel Hill, NC',
       period: 'January 2024 - Present',
       description: 'Conduct technical maintenance and improvements on makerspace equipment and space to ensure optimal performance and accessibility for users. Lead hands-on workshops and training sessions, allowing students and faculty to successfully utilize complex tools and technology. Provide personalized technical support and guidance, encouraging a collaborative environment for creative projects and innovation.'
     },
     {
-      id: 2,
+      id: 3,
       title: 'Software Engineer Intern',
       company: 'Computer Science Experience Lab UNC, Chapel Hill, NC',
       period: 'August 2024 - December 2024',
       description: 'Utilize technical leadership skills, emphasizing collaborative design, implementation, and delivery of human-centered software systems. Improve spoken and written technical communications to specific audiences of stakeholders, with an emphasis on collaborators. Design, specify, construct, verify iterating on, and operate production-grade software systems in a team setting.'
     },
     {
-      id: 3,
+      id: 4,
       title: 'Barista',
       company: 'Starbucks, Wilmington, NC',
       period: 'October 2021 - August 2022',
